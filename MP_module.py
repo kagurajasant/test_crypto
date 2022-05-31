b9219ead4e5e19af7e0fbe6ac736ab8c69d9e805
Sachan <ssachan@visteon.com>
2022-05-31 12:13:04 -0400

    MP done, CMAC next

--- MP_module.py
+++ MP_module.py
@@ -2,14 +2,31 @@ from aes_module import AES_ECB
 from math import floor
 
 
-def Miyaguchi_Preneel(key:bytes, message:bytes):
-	m = bin(int(message.hex(),16))[2:]
+def Miyaguchi_Preneel(message:bytes):
+	print('lmessage',len(message))
+	m = bin(int(message.hex(),16))[2:].zfill(len(message)*8)
+	#m = bin(int(message.hex(),16))[2:]
+	print('lmessage: {0},,,{1}'.format(len(m), m))
 	l = len(m)
-	print("l = ", bin(l)[2:])
-	if l % 128 != 0:
-		k = '0'*((88 - l - 1) % 128)
-		print("k = ",len(k))
-		n = bin(l).zfill(40)
-		print("n=",n)
+
+	print("l = {0}, {1}".format(l, bin(l)[2:]))
+	k = '0'*((88 - l - 1) % 128)
+	print("k = ",len(k))
+	l_pad = bin(l)[2:].zfill(40)
+	print("l_pad=",l_pad)
+	m_fin = m + '1' + k + l_pad
+	print("m_fin=",m_fin, len(m_fin))
+	out = 0x0.to_bytes(16, "big")
+	o = []
+	o.append(AES_ECB(out))
+	for i in range (0, len(m_fin)//128):
+		m_block = m_fin[i*128 : (i+1)*128]
+		output = o[i].encrypt(int(m_block,2).to_bytes(16,'big'))
+		out = (int(out.hex(), 16) ^ int(output.hex(), 16) ^ int(m_block, 2)).to_bytes(16, 'big')
+		o.append(AES_ECB(out))
+	print(out.hex())
+
+		
+
 	
 
--- test_mp.py
+++ test_mp.py
@@ -1,5 +1,7 @@
+from email import message
 from MP_module import Miyaguchi_Preneel
 
-key = b'\xab'
-message = b'\x7b\xab'
-Miyaguchi_Preneel(key, message)
+# key = 0xdfa66747de9ae63030ca32611497c827.to_bytes(16, "big")
+# message = b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xfe'
+message = 0x6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e51
+Miyaguchi_Preneel(message.to_bytes(32, "big"))
