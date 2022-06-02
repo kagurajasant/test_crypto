3c0b85c7831dd0aa100db12c7ba1537d94cf1721
Sachan <ssachan@visteon.com>
2022-06-02 09:25:45 -0400

    formalize CMAC test

--- cmac_module.py
+++ cmac_module.py
@@ -1,3 +1,4 @@
+import re
 from aes_module import AES_ECB
 
 def left_shift_128(i:int):
@@ -22,5 +23,38 @@ def SUBK(k:bytes):
 	
 	print('K2 : {0}'.format(hex(K2)[2:]))
 
+	return K1, K2
+
 def CMAC(k:bytes, m:bytes, tlen:int):
-	
+	K1, K2 = SUBK(k)
+	b = 128
+	mlen = len(bin(int(m.hex(),16))[2:].zfill(len(m)*8))
+	# print('m_len:', mlen)
+	m_bit = bin(int(m.hex(),16))[2:].zfill(len(m)*8)
+	# print('m_bit:', m_bit)
+	if int(m.hex(), 16) == 0:
+		n = 1
+	else:
+		n = -1 * (-mlen//b)
+
+	if mlen %128 != 0:
+		m_bit += '1' + '0'*(128 - (mlen+1)%128)
+		m_final = bin(int(m_bit,2) ^ K2)[2:].zfill(len(m_bit))
+		# print('m_final:{0}, len:{1}'.format(m_final, len(m_bit)))
+
+	else:
+		m_final = bin(int(m_bit,2) ^ K1)[2:].zfill(len(m_bit))
+
+	# print('final_len:',len(m_final), type(K1))
+	prev_out = b'\x00'
+
+	for i in range (0, len(m_final)//128):
+		AES_cipher_object = AES_ECB(k)
+		m_block = m_final[i*128 : (i+1)*128]
+		# print('m_block=', m_block)
+		ciphertext = AES_cipher_object.encrypt((int(m_block,2)^int(prev_out.hex(),16)).to_bytes(16,'big'))
+		prev_out = ciphertext
+
+	ret_val = bin(int.from_bytes(prev_out, "big"))[2:tlen+2]
+	return ret_val
+	# print('prevout:',prev_out.hex())
--- test_cmac.py
+++ test_cmac.py
@@ -1,4 +1,6 @@
 import cmac_module
 
 key = 0x2b7e151628aed2a6abf7158809cf4f3c.to_bytes(16, "big")
-cmac_module.SUBK(key)
+# m = 0x6bc1bee22e409f96e93d7e117393172a.to_bytes(16, 'big')
+m = 0x6bc1bee22e409f96e93d7e117393172aae2d8a571e03ac9c9eb76fac45af8e5130c81c46a35ce411.to_bytes(40, 'big')
+print(hex(int(cmac_module.CMAC(key, m, 128),2))[2:])
